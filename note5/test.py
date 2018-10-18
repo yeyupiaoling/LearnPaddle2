6ab49250ba1c0fb42d2d123dea26f4e692dc8254
yeyupiaoling <yeyupiaoling@foxmail.com>
2018-10-18 08:34:54 +0800

    update rnn

--- note5/test.py
+++ note5/test.py
@@ -3,12 +3,14 @@ import paddle.dataset.imdb as imdb
 import paddle.fluid as fluid
 import numpy as np
 
+
 def rnn_net(ipt, input_dim):
     emb = fluid.layers.embedding(input=ipt, size=[input_dim, 128], is_sparse=True)
+    sentence = fluid.layers.fc(input=emb, size=128, act='tanh')
 
     rnn = fluid.layers.DynamicRNN()
     with rnn.block():
-        word = rnn.step_input(emb)
+        word = rnn.step_input(sentence)
         prev = rnn.memory(shape=[512])
         hidden = fluid.layers.fc(input=[word, prev], size=512, act='relu')
         rnn.update_memory(prev, hidden)
