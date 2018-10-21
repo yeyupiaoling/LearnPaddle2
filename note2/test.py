f8e4167680aa0a70d8c8275f430311d5e8802402
yeyupiaoling <yeyupiaoling@foxmail.com>
2018-10-21 17:01:29 +0800

    update note5 network

--- note2/test.py
+++ note2/test.py
@@ -1,24 +1,25 @@
 import paddle.fluid as fluid
+import numpy as np
 
-# 定义两个维度是1，值也是1的张量
-x1 = fluid.layers.fill_constant(shape=[1], value=1, dtype='int64')
-x2 = fluid.layers.fill_constant(shape=[1], value=1, dtype='int64')
+# 定义两个张量
+a = fluid.layers.create_tensor(dtype='int64', name='a')
+b = fluid.layers.create_tensor(dtype='int64', name='b')
 
 # 将两个张量求和
-y = fluid.layers.sum(x=[x1, x2])
+y = fluid.layers.sum(x=[a, b])
 
 # 创建一个使用CPU的解释器
 place = fluid.CPUPlace()
 exe = fluid.executor.Executor(place)
 # 进行参数初始化
-exe.run(fluid.default_main_program())
+exe.run(fluid.default_startup_program())
+
+a1 = np.array([3]).astype('int64')
+b1 = np.array([1]).astype('int64')
 
 # 进行运算，并把y的结果输出
-# result = exe.run(program=fluid.default_main_program(),
-#                  fetch_list=[y])
-# 输出计算结果
-# print("1+1 =", result[0][0])
-
-
-result = exe.run(program=fluid.default_main_program(),
-                 fetch_list=[y])
+result, r = exe.run(program=fluid.default_main_program(),
+                    feed={'a': a1, 'b': b1},
+                    fetch_list=[y, b])
+print(result)
+print(r)
