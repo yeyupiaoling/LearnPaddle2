b39f40e64bd6a9279fdc3ce8d982ab5b6f4735e6
yeyupiaoling <yeyupiaoling@foxmail.com>
2018-10-22 10:01:41 +0800

    fix note5

--- note5/test.py
+++ note5/test.py
@@ -6,13 +6,13 @@ import numpy as np
 
 def rnn_net(ipt, input_dim):
     emb = fluid.layers.embedding(input=ipt, size=[input_dim, 128], is_sparse=True)
-    sentence = fluid.layers.fc(input=emb, size=512, act='tanh')
+    sentence = fluid.layers.fc(input=emb, size=128, act='tanh')
 
     rnn = fluid.layers.DynamicRNN()
     with rnn.block():
         word = rnn.step_input(sentence)
-        prev = rnn.memory(shape=[512])
-        hidden = fluid.layers.fc(input=[word, prev], size=512, act='relu')
+        prev = rnn.memory(shape=[128])
+        hidden = fluid.layers.fc(input=[word, prev], size=128, act='relu')
         rnn.update_memory(prev, hidden)
         rnn.output(hidden)
 
@@ -27,9 +27,9 @@ def lstm_net(ipt, input_dim):
     emb = fluid.layers.embedding(input=ipt, size=[input_dim, 128], is_sparse=True)
 
     # 第一个全连接层
-    fc1 = fluid.layers.fc(input=emb, size=512)
+    fc1 = fluid.layers.fc(input=emb, size=128)
     # 进行一个长短期记忆操作
-    lstm1, _ = fluid.layers.dynamic_lstm(input=fc1, size=512)
+    lstm1, _ = fluid.layers.dynamic_lstm(input=fc1, size=128)
 
     # 第一个最大序列池操作
     fc2 = fluid.layers.sequence_pool(input=fc1, pool_type='max')
