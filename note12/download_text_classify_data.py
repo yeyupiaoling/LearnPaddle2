7c46f52eddb514421a2bc5b3619cf8ada69878b5
yeyupiaoling <yeyupiaoling@foxmail.com>
2019-02-18 17:01:10 +0800

    update note12 download data

--- note12/download_text_classify_data.py
+++ note12/download_text_classify_data.py
@@ -38,9 +38,26 @@ def get_data(tup):
     # 分类新闻的访问参数
     t = int(time.time() / 10000)
     t = random.randint(6 * t, 10 * t)
-    querystring = {"category": tup[2], "max_behot_time": t}
+    querystring = {"category": tup[2], "max_behot_time": t, "last_refresh_sub_entrance_interval": "1524907088", "loc_mode": "5",
+                   "tt_from": "pre_load_more", "cp": "51a5ee4f38c50q1", "plugin_enable": "0", "iid": "31047425023",
+                   "device_id": "51425358841", "ac": "wifi", "channel": "tengxun", "aid": "13",
+                   "app_name": "news_article", "version_code": "631", "version_name": "6.3.1",
+                   "device_platform": "android",
+                   "ab_version": "333116,297979,317498,336556,295827,325046,239097,324283,170988,335432,332098,325198,336443,330632,297058,276203,286212,313219,328615,332041,329358,322321,327537,335710,333883,335102,334828,328670,324007,317077,334305,280773,335671,319960,333985,331719,336452,214069,31643,332881,333968,318434,207253,266310,321519,247847,281298,328218,335998,325618,333327,336199,323429,287591,288418,260650,326188,324614,335477,271178,326588,326524,326532",
+                   "ab_client": "a1,c4,e1,f2,g2,f7", "ab_feature": "94563,102749", "abflag": "3", "ssmix": "a",
+                   "device_type": "MuMu", "device_brand": "Android", "language": "zh", "os_api": "19",
+                   "os_version": "4.4.4", "uuid": "008796762094657", "openudid": "b7215ea70ca32066",
+                   "manifest_version_code": "631", "resolution": "1280*720", "dpi": "240",
+                   "update_version_code": "6310", "_rticket": "1524907088018", "plugin": "256"}
+
+    headers = {
+        'cache-control': "no-cache",
+        'postman-token': "26530547-e697-1e8b-fd82-7c6014b3ee86",
+        'User-Agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.4; MuMu Build/V417IR) NewsArticle/6.3.1 okhttp/3.7.0.2'
+    }
+
     # 进行网络请求
-    response = requests.request("GET", url, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
     # 获取返回的数据
     new_data = json.loads(response.text)
     with open('datasets/news_classify_data.txt', 'a', encoding='utf-8') as fp:
